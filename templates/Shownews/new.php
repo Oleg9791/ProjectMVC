08a9d645f007afe804dbfdf1f36a9c77f9183eae
Oleg9791 <oleg9791@tut.by>
2021-08-20 16:30:32 +0300

    changes

--- templates/Shownews/new.php
+++
@@ -1,9 +0,0 @@
-<h1><?= $this->data['new']['caption'] ?></h1>
-<img src="public/images/news/<?= $this->data['new']['id'] . '_' . $this->data['new']['picture'] ?>">
-<div><?= $this->data['new']['text'] ?></div>
-<hr>
-<?php
-//print_r($this->data);
-foreach ($this->data['comments'] as $row) {
-    echo "<i>$row[text]</i><b>$row[name]</b><br>";
-}
